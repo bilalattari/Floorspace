fc1eb192205c1bac89f4e40b23b4f3f0959aefad
bilalattari <attari1235@gmail.com>
2020-05-18 06:12:06 +0500

    Home page done

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,32 +1,53 @@
 /* eslint-disable */
 
-import React, { Component } from 'react';
-import { Button, Icon } from 'react-native-elements';
-import { TouchableOpacity, ActivityIndicator, View, StyleSheet } from 'react-native';
-import { themeColor, pinkColor } from '../Constant/index';
+import React, {Component} from 'react';
+import {Button, Icon} from 'react-native-elements';
+import {
+  TouchableOpacity,
+  ActivityIndicator,
+  View,
+  StyleSheet,
+} from 'react-native';
+import {themeColor, pinkColor} from '../Constant/index';
 import Text from './Text';
-export default Footer = ({ }) =>
-    <View style={styles.footerView}>
-        <Icon type={'material-community'} name={'home-outline'} color={'#fff'} />
-        <Icon type={'material-community'} name={'filter-outline'} color={'#fff'} />
-        <TouchableOpacity style={styles.middleButton}>
-            <Icon type={'font-awesome'} name={'plus'} color={'#000'} containerStyle = {{paddingTop : 3}} />
-        </TouchableOpacity>
-        <Icon type={'entypo'} name={'box'} color={'#fff'} />
-        <Icon type={'material-community'} name={'delete-outline'} color={'#fff'} />
-    </View>
-
-
-
+export default Footer = ({}) => (
+  <View style={styles.footerView}>
+    <Icon type={'material-community'} name={'home-outline'} color={'#fff'} />
+    <Icon type={'material-community'} name={'filter-outline'} color={'#fff'} />
+    <TouchableOpacity style={styles.middleButton}>
+      <Icon
+        type={'font-awesome'}
+        name={'plus'}
+        color={'#000'}
+        containerStyle={{paddingTop: 3}}
+      />
+    </TouchableOpacity>
+    <Icon type={'entypo'} name={'box'} color={'#fff'} />
+    <Icon type={'material-community'} name={'delete-outline'} color={'#fff'} />
+  </View>
+);
 
 const styles = StyleSheet.create({
-    footerView: {
-        width: '90%', height: 50, borderRadius: 25, alignSelf: "center", paddingHorizontal: 12,
-        backgroundColor: "#000", position: "absolute", bottom: 12, flexDirection: "row",
-        justifyContent: "space-around", alignItems: "center"
-    },
-    middleButton: {
-        marginTop: -41, height: 50, width: 50, justifyContent: "center", alignItems: "center",
-        backgroundColor: "#fff", borderRadius: 125
-    }
-})
+  footerView: {
+    width: '90%',
+    height: 50,
+    borderRadius: 25,
+    alignSelf: 'center',
+    paddingHorizontal: 12,
+    backgroundColor: '#000',
+    position: 'absolute',
+    bottom: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  middleButton: {
+    marginTop: -41,
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 125,
+  },
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet, View, Image, ScrollView, TouchableOpacity, FlatList
-} from 'react-native';
-import { themeColor } from '../Constant/index'
-import Input from '../Component/Input'
-import Button from '../Component/Button'
-import Text from '../Component/Text'
-import { CheckBox } from 'react-native-elements'
-import { Icon, SearchBar } from 'react-native-elements';
-import ImagePicker from 'react-native-image-crop-picker';
-import Footer from '../Component/Footer'
-function Home({ navigation }) {
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, FlatList, Image} from 'react-native';
+import {themeColor} from '../Constant/index';
+import Button from '../Component/Button';
+import Text from '../Component/Text';
+import {Icon, SearchBar} from 'react-native-elements';
+import Footer from '../Component/Footer';
+import Header from '../Component/header';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+const List = [
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Active',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Cold',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Not Interesr',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Active',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Cold',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Not Interesr',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Active',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Cold',
+  },
+  {
+    name: 'Jon Jones',
+    address: '20 Chelsea Way, Mapleton',
+    created: '01/ 01/ 2020',
+    createdTime: '10:54 am',
+    modified: '03/ 01/ 2020',
+    modifiedTime: '11:54 pm',
+    status: 'Not Interesr',
+  },
+];
+
+function Home({navigation}) {
   const [search, onChangeSearch] = React.useState('');
   // const [checked, onChangeChecked] = React.useState(false);
   // const [password, onChangePassword] = React.useState('');
@@ -19,15 +100,89 @@ function Home({ navigation }) {
     setLoader(boolean);
   };
   return (
-    <View style={{ flex: 1, backgroundColor: "#E5E5E5" }}>
-      <Text text={"HOme"} color={themeColor} />
+    <View style={{flex: 1, backgroundColor: '#E5E5E5'}}>
+      <Header />
+      <ScrollView>
+        <Text
+          color={themeColor}
+          text={'Recent Project'}
+          bold={true}
+          font={21}
+          style={styles.heading}
+        />
+        <FlatList
+          data={List}
+          renderItem={({item, index}) => (
+            <View
+              style={styles.textView}>
+              <View style={styles.itemContainer}>
+                <Text color={themeColor} text={item.name} font={18} />
+                <Text color={themeColor} text={item.address} font={15} />
+                <Text
+                  color={themeColor}
+                  text={`Created : ${item.created}   ${item.createdTime}`}
+                  font={15}
+                />
+                <Text
+                  color={themeColor}
+                  text={`Created : ${item.modified}   ${item.modifiedTime}`}
+                  font={15}
+                />
+              </View>
+              <TouchableOpacity style={styles.button}>
+                <Text
+                  text={item.status}
+                  color={
+                    item.status === 'Active'
+                      ? '#43C71B'
+                      : item.status === 'Cold'
+                      ? '#1BBAC7'
+                      : '#D96034'
+                  }
+                  font={14}
+                  bold={true}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  source={require('../assets/dots.png')}
+                  height={20}
+                  style={{paddingHorizontal: 8}}
+                  resizeMode={'contain'}
+                />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </ScrollView>
       <Footer />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {padding: 12},
+  itemContainer: {
+    flex: 1,
+  },
+  textView : {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    minHeight: 80,
+    marginVertical: 5,
+    padding: 5,
+    paddingLeft : 12,
+  },
+  button: {
+    padding: 12,
+    paddingHorizontal: 15,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 6,
+    backgroundColor: '#F1F1F1',
+  },
+});
 
-})
-
-export default Home
+export default Home;
